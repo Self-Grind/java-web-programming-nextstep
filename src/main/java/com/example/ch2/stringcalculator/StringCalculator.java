29861e6490ad0a839dfed94ea3181b400b698501
LEEHYUNDONG <l.steve10@yahoo.com>
2023-06-10 13:12:39 +0900

    feat: String Calculator - addV1 기능만 구현한 코드 - addV2 리팩토링, 한개의 기능만 담도록 함수로 뽑아서 개발

--- src/main/java/com/example/ch2/stringcalculator/StringCalculator.java
+++ src/main/java/com/example/ch2/stringcalculator/StringCalculator.java
@@ -1,4 +1,95 @@
 package com.example.ch2.stringcalculator;
 
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
+    /**
+     * 초기 작성 - step 1
+     * 숫자 계산
+     * regex -
+     */
+    public int add(String text) {
+        String num = "";
+        String pattern = ":,";
+
+        if (text == null || text.isEmpty())
+            return 0;
+        // 만약 커스텀 구분자가 있다면 커스텀 구분자까지 pattern에 포함한다.
+        if (text.contains("\n")) {
+            num = text.substring(text.indexOf("\n") + 1);
+            pattern += text.substring(text.indexOf("/") + 2, text.indexOf("\n"));
+        }
+        //음수가 포함됐을 경우 RuntimeException
+        if (num.contains("-")) throw new RuntimeException();
+
+        System.out.println("num = " + num);
+        pattern = "[" + pattern + "]";
+        String[] lst = num.split(pattern);
+        // 계산
+        int answer = 0;
+        for (String s : lst) {
+            answer += Integer.parseInt(s);
+        }
+//        System.out.println("answer = " + answer);
+        return answer;
+    }
+
+    /**
+     * addV2
+     */
+    /**
+     * 초기 작성 - step 1
+     * 숫자 계산
+     * regex -
+     * 리팩토링
+     */
+    public int addV2(String text) {
+        // 빈문자열
+        if (isBlank(text))
+            return 0;
+
+        return sum(toInts(split(text)));
+    }
+
+    private boolean isBlank(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = toPositive(values[i]);
+        }
+        return numbers;
+    }
+
+    private int sum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    private String[] split(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String delimeter = m.group(1);
+            return m.group(2).split(delimeter);
+        }
+        return text.split("[,:]");
+    }
+
+    private int toPositive(String value) {
+        int number = Integer.parseInt(value);
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
 }
---
+++ src/test/java/com/example/ch2/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,53 @@
+package com.example.ch2.stringcalculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StringCalculatorTest {
+    private static StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setup(){
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    void addText(){
+        String input = "//.\n1,2.3,4:5";
+        Assertions.assertThat(stringCalculator.add(input)).isEqualTo(15);
+
+    }
+
+    @Test
+    void addV2_빈문자열(){
+        Assertions.assertThat(stringCalculator.addV2("")).isEqualTo(0);
+        Assertions.assertThat(stringCalculator.addV2(null)).isEqualTo(0);
+    }
+    @Test
+    void addV2_숫자하나(){
+        Assertions.assertThat(stringCalculator.addV2("1")).isEqualTo(1);
+    }
+
+    @Test
+    public void addV2_쉼표() throws Exception {
+        assertEquals(stringCalculator.addV2("1,2:3"), 6);
+
+    }
+
+    @Test
+    public void addV2_여러구분자() throws Exception{
+        assertEquals(stringCalculator.addV2("//;\n1;2;3"), 6);
+    }
+
+    @Test
+    public void add_negative() throws Exception {
+        assertThrows(RuntimeException.class, ()->{
+            stringCalculator.addV2("-1,2,3");
+        });
+    }
+
+}
